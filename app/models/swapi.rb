ed83930112d2f02e4000ec9a4bdde05bf238ded0
Andrew Baik <and.baik@gmail.com>
2016-01-27 14:02:02 -0500

    refactored swapi api code

--- app/models/swapi.rb
+++ app/models/swapi.rb
@@ -3,49 +3,33 @@ class Swapi
   include HTTParty
 
   BASE_URI = "http://swapi.co/api"
-  
+
   def initialize
-     
+    @nouns = {
+      "people" => JSON.parse(HTTParty.get("#{BASE_URI}/people/").body)["count"],
+      "planets" => JSON.parse(HTTParty.get("#{BASE_URI}/planets/").body)["count"],
+      "vehicles" => JSON.parse(HTTParty.get("#{BASE_URI}/vehicles/").body)["count"],
+      "starships" => JSON.parse(HTTParty.get("#{BASE_URI}/starships/").body)["count"],
+      "species" => JSON.parse(HTTParty.get("#{BASE_URI}/species/").body)["count"]
+    }
   end
 
-  def people
-    random = rand(1..87)
-    response = HTTParty.get("#{BASE_URI}/people/#{random}")
-    #puts response
-    person_name = JSON.parse(response.body)["name"]
-  end
-
-  def planet
-    random = rand(1..61)
-    response = HTTParty.get("#{BASE_URI}/planets/#{random}")
-    planet_name = JSON.parse(response.body)["name"]
-  end
-
-  def vehicle
-    vehicle_name = nil
-
-    until !vehicle_name.nil?
-      random = rand(1..39)
-      puts "Random in vehicle is #{random}"
-      response = HTTParty.get("#{BASE_URI}/vehicles/#{random}")
-      vehicle_name = JSON.parse(response.body)["name"]
-    end  
-    vehicle_name
-  end
-
-  def starship
-    random = rand(1..37)
-    response = HTTParty.get("#{BASE_URI}/starships/#{random}")
-    starship_name = JSON.parse(response.body)["name"]
-  end
-
-  def species
-    random = rand(1..37)
-    response = HTTParty.get("#{BASE_URI}/species/#{random}")
-    species_name = JSON.parse(response.body)["name"]
+  def get_noun(param)
+    attribute = nil
+    until !attribute.nil?
+      random = rand(1..@nouns[param].to_i)
+      response = HTTParty.get("#{BASE_URI}/#{param}/#{random}")
+      attribute = JSON.parse(response.body)["name"]
+    end
+    attribute
   end
 
   def generate_prompt
+    people = get_noun("people")
+    planet = get_noun("planets")
+    vehicle = get_noun("vehicles")
+    starship = get_noun("starships")
+    species = get_noun("species")
     "#{people} is at #{planet} riding on a #{vehicle} heading towards a #{starship} filled with #{species}s"
-  end  
+  end
 end
