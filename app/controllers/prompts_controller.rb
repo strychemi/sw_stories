ddd0d96b84643acebc3d83fc45cd73f6dd8f1e22
Deepa Kackar <dkackar99@yahoo.com>
2016-01-27 14:46:33 -0800

    changed for passing the prompt

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -5,8 +5,16 @@ class PromptsController < ApplicationController
   end
 
   def index
-    @user = User.find(:id)
-    @prompts = Prompts.find(current_user)
+    #@user = User.find(:id)
+    @prompts = Prompt.find_all(current_user)
   end
 
+  def create
+    @prompt = Prompt.new(text: params[:prompt], user_id: current_user.id)
+    if @prompt.save
+      redirect_to user_prompts_path 
+    else
+     flash[:error] = "Prompt not saved!"
+    end  
+  end 
 end
