2f07dfc94970987554240c76e7138d07347dc4c7
Andrew Baik <and.baik@gmail.com>
2016-01-27 12:55:19 -0500

    basic user signup, signin, logout works

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root "welcomes#index"
 
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
 
   resources :users do
     resources :prompts
