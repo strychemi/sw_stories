ef271e8f325f95eb397eb56577a25b0e596f68e6
Andrew Baik <and.baik@gmail.com>
2016-01-27 12:52:30 -0500

    working on basic logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  root "welcomes#index"
+
   devise_for :users
+
+  resources :users do
+    resources :prompts
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127170953) do
+ActiveRecord::Schema.define(version: 20160127172223) do
+
+  create_table "prompts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
