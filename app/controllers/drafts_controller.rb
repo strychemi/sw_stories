fc002e66663c472c5d43c31f9a467de3218551c2
Deepa Kackar <dkackar99@yahoo.com>
2016-01-27 16:15:41 -0800

    Added some links to show all or delete

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -14,7 +14,7 @@ class DraftsController < ApplicationController
     @draft = Draft.new(story: params[:draft], prompt_id: params[:prompt_id])
     if @draft.save
       flash[:success] = "Draft saved"
-      redirect_to user_prompt_draft_path(id: params[:id])
+      redirect_to user_prompt_draft_path(id: Draft.last.id, prompt_id: params[:prompt_id])
     else
       flash.now[:error] = "Draft not saved"
       render :new
